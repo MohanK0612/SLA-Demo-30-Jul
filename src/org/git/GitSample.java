5db1320d3b989d47aa5a44914fe60b0dd0a11c2b
MohanK0612 <ermohankm@gmail.com>
2025-07-30 13:33:23 +0530

    Project Completed

---
+++ src/org/git/GitSample.java
@@ -0,0 +1,29 @@
+package org.git;
+
+public class GitSample {
+	
+	private void ClassA() {
+		System.out.println("Java");
+		
+	}
+private void ClassB() {
+	System.out.println("Selenium");
+		
+	}
+private void ClassC() {
+	System.out.println("Framework");
+	
+}
+	
+	public static void main(String[] args) {
+		
+		GitSample gs = new GitSample();
+		gs.ClassA();
+		gs.ClassB();
+		gs.ClassC();
+		
+		
+		
+	}
+
+}
