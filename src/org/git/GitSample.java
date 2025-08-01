4d49135456c259226281de1593551beab69e6466
MohanK0612 <ermohankm@gmail.com>
2025-08-01 12:59:11 +0530

    Updated

--- src/org/git/GitSample.java
+++ src/org/git/GitSample.java
@@ -14,13 +14,17 @@ private void ClassC() {
 	System.out.println("Framework");
 	
 }
+private void ClassD() {
+	System.out.println("GIT");
 	
+}
 	public static void main(String[] args) {
 		
 		GitSample gs = new GitSample();
 		gs.ClassA();
 		gs.ClassB();
 		gs.ClassC();
+		gs.ClassD();
 		
 		
 		
